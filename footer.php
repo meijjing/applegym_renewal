e3ad11965c7816f31aec1dabf1001e14232c3393
meijjing <kbunny711@naver.com>
2021-07-14 20:53:47 +0900

    포트폴리오용 출처 표기함

--- footer.php
+++ footer.php
@@ -73,7 +73,13 @@
     </div>
 
     <div class="copy">
-      <p>COPYRIGHT © 2020 Applegym All Rights Reserved</p>
+      <p>
+        COPYRIGHT © 2020 Applegym All Rights Reserved
+      </p>
+      <p>
+        본 사이트는 개인 포트폴리오 사이트로 제작되어 상업적 목적이 아닙니다.
+        사용된 일부 이미지 및 폰트 등은 별도의 출처가 있음을 밝혀 드립니다.
+      </p>
     </div>
 
   </div><!-- footer_bg-->
